9182b83ab7243519c4148b889425a8a8a25c828f
German <plaga701@gmail.com>
2010-05-18 22:35:20 -0430

    - utils.*    . Agregada funcion que imprime de manera ordenada      un Packing    . Agregada funcion de busqueda lineal    . Agregada funcion que busca un item en un vector de Placement

--- utils.h
+++ utils.h
@@ -3,3 +3,9 @@
 int compareHeight(const void *, const void *);
 
 int * combinations(int,int,int*);
+
+int linS(const int*,int,int);
+
+int searchItem(const vector<Placement>,int);
+
+void printPack(const Packing);
