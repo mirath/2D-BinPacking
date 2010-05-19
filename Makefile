6a0f5f320b262860d33050aafc6134a0ac35263f
Lorenzo Fundaró García <lorenzo@ac.labf.usb.ve>
2010-05-19 16:35:38 -0430

    - Se implementó la lectura de archivo - Commit antes de cambiar al branch que no compila

--- Makefile
+++ Makefile
@@ -1,9 +1,20 @@
 CXX	:= g++
 CXXFLAGS := -g
-OBJECTS := 
+OBJECTS := binPacking2D.o FiniteBestStrip.o utils.o
 
-binpacking2D: $(OBJECTS)
-	$(CXX) $(CXXFLAGS) $(OBJECTS)
+binPacking2D: $(OBJECTS)
+	$(CXX) $(CXXFLAGS) $(OBJECTS) -o binPacking2D
+
+binPacking2D.o: binPacking2D.cpp
+	$(CXX) $(CXXFLAGS) -c binPacking2D.cpp
+
+FiniteBestStrip.o: FiniteBestStrip.cpp
+	$(CXX) $(CXXFLAGS) -c FiniteBestStrip.cpp FiniteBestStrip.h 
+
+utils.o: utils.cpp
+	$(CXX) $(CXXFLAGS) -c utils.cpp utils.h
+
+
 
 clean:
 	rm -rf *.o *.out semantic.cache *.gch *~ *.tex.backup
---
+++ binPacking2D.cpp
@@ -0,0 +1,53 @@
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <string>
+#include <istream>
+#include "utils.h"
+#include "FiniteBestStrip.h"
+#include "dataStructs.h"
+
+int main(int argc, char *argv[]) {
+  ifstream instance(argv[1]);
+  char * line = (char *) malloc(sizeof(char)*60);
+  if (instance.is_open()) {
+    int clase;
+    int nItems;
+    vector<Item> items;
+    int Hbin;
+    int Wbin;
+    while (!instance.eof()) {
+      // Problem class 
+      instance.getline(line, 60);
+      sscanf(line," %d", &clase);
+      // Number of items
+      instance.getline(line, 60);
+      sscanf(line," %d", &nItems);
+      // Non useful parameter
+      instance.getline(line, 60);
+      // Height and Width of bin
+      instance.getline(line, 60);
+      sscanf(line," %d %d", &Hbin, &Wbin);
+      // Starting to loop over items
+      int width;
+      int height;
+      for(int i = 0; i < nItems; i++) {
+        instance.getline(line, 60);
+        sscanf(line," %d %d", &height, &width);
+        items.push_back((Item) {width, height, i, false});
+      }
+      // Whiteline after reading last Item
+      instance.getline(line, 60);
+    }
+    instance.close();
+    Packing result = FBS(items, Hbin, Wbin);
+    printPack(result);
+  }
+  else {
+    cout << "Error leyendo instacia" << endl;
+    exit(0);
+  }
+  free(line);
+
+}
