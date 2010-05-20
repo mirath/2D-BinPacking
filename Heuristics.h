f56a0e1caccf4ef94a476286561cf99da5446adf
German <plaga701@gmail.com>
2010-05-20 12:59:09 -0430

    - Comentados Heuristics.h, LocalSearch.h y utils.h

--- Heuristics.h
+++ Heuristics.h
@@ -5,12 +5,25 @@
 #include <vector>
 using namespace std;
 
+//IN: - Packing
+//    - int*: arreglo de bins
+//    - int: bin prohibido
+//OUT: - Vector<Item>: items del Packing que estan
+//                     en los bins del arreglo
 vector<Item>* getItems(int*,int,int,Packing*);
 
+
+//Sincroniza la informacion de los items del
+//segundo packing con los del primero
 Packing * update(Packing,Packing*,int*,int,int);
 
+
+//Decide que packing es "mejor" dado que ambos
+//usan el mismo numero de bins
 Packing* breakTie(Packing*,Packing*,int,int);
 
+//Busqueda Primer Mejor
 int HFirstBest(int,Packing*,int,int,int);
 
+//Busqueda Mejor Mejor
 int HBestBest(int,Packing*,int,int,int);
--- utils.h
+++ utils.h
@@ -14,21 +14,39 @@ void init_visited(vector <Item> &);
 void init_vi_strip(vector <Strip> &);
 
 
-//Heuristics
+/*===Heuristics===*/
+
+//Devuelve la posicion de un item en un 
+//vector de Placement
 int searchItem(const vector<Placement>,int);
 
+//Devuelve la posicion de un bin en un 
+//vector de Placement
 int searchBin(const vector<Placement>,int);
 
+//Busqueda lineal
 bool linS(int*,int,int);
 
+//Devuelve la posicion de un double en
+//una lista de double
 int linSlist(list<double>,double);
 
 long factorial(int i);
 
+
+//Factorial superior
+long factorial_n(int i,int n);
+
+//Calcula el numero de combinaciones (n en k)
 long comb(int n, int k);
 
+//Dado un arreglo con una combinacion, devuelve la
+//proxima combinacion
 int * combinations(int,int,int*);
 
+//Dada un alista de doubles, devueve el valor
+//del elemento que estaria en la nth posicion
+//si la lista estuviera ordenada
 double nth(list<double>*,int);
 
 //Others
