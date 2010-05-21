686e74b92b778dc63924683a96962a2c3496ebe6
Lorenzo Fundaró García <lorenzo@ac.labf.usb.ve>
2010-05-21 17:16:09 -0430

    - se arreglo bug en función de comparación. Sort de c++ no acepta >=.

--- utils.cpp
+++ utils.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool compareHeight(Item a, Item b) {
   // We add the outer minus sign to get nonincreasing 
   // ordering.
-  return (a.height >= b.height);
+  return (a.height > b.height);
 }
 
 void init_visited(vector <Item> &items) {
